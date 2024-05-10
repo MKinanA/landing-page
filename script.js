3e984aa36c7db244c02792d6167813fb873f804e
MKinanA <mkinanahsan@gmail.com>
2024-05-10 09:02:25 +0700

    Modified background fade-out behaviour

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 console.log('Script initialized');
 document.addEventListener('DOMContentLoaded', (_event) => {
     console.log('DOMContentLoaded');
-    updateBodyTransparency = () => {document.body.style.background = `rgba(7, 7, 15, ${window.scrollY <= window.innerHeight? (window.scrollY / window.innerHeight) + 0.25 : 1})`;}
+    updateBodyTransparency = () => {document.body.style.background = `rgba(7, 7, 15, ${window.scrollY >= (window.innerHeight / 4)? (window.scrollY <= window.innerHeight? window.scrollY / window.innerHeight : 1) : 0.25})`;}
     updateBodyTransparency()
     window.addEventListener('scroll', (_event) => {
         console.log(window.scrollY);
