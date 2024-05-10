c85ac95e09264df16f02472282f7f36880a78372
MKinanA <mkinanahsan@gmail.com>
2024-05-10 10:39:54 +0700

    Added hamburger menu button functionality, and some other modifications

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 console.log('Script initialized');
 document.addEventListener('DOMContentLoaded', (_event) => {
     console.log('DOMContentLoaded');
-    updateBodyTransparency = () => {document.body.style.background = `rgba(7, 7, 15, ${window.scrollY >= (window.innerHeight / 4)? (window.scrollY <= window.innerHeight? window.scrollY / window.innerHeight : 1) : 0.25})`;};
+    updateBodyTransparency = () => {document.querySelector('body > main').style.background = `rgba(7, 7, 15, ${window.scrollY >= (window.innerHeight / 4)? (window.scrollY <= window.innerHeight? window.scrollY / window.innerHeight : 1) : 0.25})`;};
     updateBodyTransparency();
     window.addEventListener('scroll', (_event) => {
         updateBodyTransparency();
+        document.querySelector('header > nav > div').classList.remove('show')
+    });
+    window.addEventListener('click', (event) => {
+        if (event.target !== document.querySelector('header > nav > button') && !document.querySelector('header > nav > button').contains(event.target)) {
+            document.querySelector('header > nav > div').classList.remove('show')
+        };
+    });
+    document.querySelector('header > nav > button').addEventListener('click', (_event) => {
+        document.querySelector('header > nav > div').classList.toggle('show');
     });
 });
