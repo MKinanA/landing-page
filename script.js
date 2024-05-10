5040cdc8d394be948e421c874410504a74d0451e
MKinanA <mkinanahsan@gmail.com>
2024-05-10 09:05:56 +0700

    Added several missing semicolons

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 console.log('Script initialized');
 document.addEventListener('DOMContentLoaded', (_event) => {
     console.log('DOMContentLoaded');
-    updateBodyTransparency = () => {document.body.style.background = `rgba(7, 7, 15, ${window.scrollY >= (window.innerHeight / 4)? (window.scrollY <= window.innerHeight? window.scrollY / window.innerHeight : 1) : 0.25})`;}
-    updateBodyTransparency()
+    updateBodyTransparency = () => {document.body.style.background = `rgba(7, 7, 15, ${window.scrollY >= (window.innerHeight / 4)? (window.scrollY <= window.innerHeight? window.scrollY / window.innerHeight : 1) : 0.25})`;};
+    updateBodyTransparency();
     window.addEventListener('scroll', (_event) => {
-        updateBodyTransparency()
+        updateBodyTransparency();
     });
-})
+});
