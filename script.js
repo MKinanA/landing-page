677f39eccb50c7386693183558043419221835c4
MKinanA <mkinanahsan@gmail.com>
2024-05-10 09:03:51 +0700

    Removed a no longer used log

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ document.addEventListener('DOMContentLoaded', (_event) => {
     updateBodyTransparency = () => {document.body.style.background = `rgba(7, 7, 15, ${window.scrollY >= (window.innerHeight / 4)? (window.scrollY <= window.innerHeight? window.scrollY / window.innerHeight : 1) : 0.25})`;}
     updateBodyTransparency()
     window.addEventListener('scroll', (_event) => {
-        console.log(window.scrollY);
         updateBodyTransparency()
     });
 })
